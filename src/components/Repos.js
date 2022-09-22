460bc4cac5429569627707641b4c5e0a5de9f90c
Luigi <luigi.drnasin12@gmail.com>
2022-09-22 16:11:53 +0200

    calculating most used languages

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,26 +5,36 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
 
-  // STEP 2 - Chart Data
-  const chartData = [
-    {
-      label: "HTML",
-      value: "20",
-    },
-    {
-      label: "CSS",
-      value: "40",
-    },
-    {
-      label: "JavaScript",
-      value: "145",
-    },
-  ];
+  //If language property is null
+  const languageCount = repos.filter((repo) => repo.language !== null);
+
+  //Empty object
+  const objectChartData = {};
+
+  //Counting language occurrences logic
+  for (const elemet of languageCount) {
+    const { language } = elemet;
+    if (objectChartData[language]) {
+      objectChartData[language] += 1;
+    } else {
+      objectChartData[language] = 1;
+    }
+  }
+
+  //Converting object to an array of objects
+  const chartData = Object.entries(objectChartData)
+    .map((item) => {
+      return { label: item[0], value: item[1] };
+    })
+    //Render 5 most used languages
+    .sort((a, b) => b.value - a.value)
+    .slice(0, 5);
 
   return (
     <section className="section">
       <Wrapper className="section-center">
-        <ExampleChart data={chartData} />
+        {/* <ExampleChart data={chartData} /> */}
+        <Pie3D data={chartData} />
       </Wrapper>
     </section>
   );
